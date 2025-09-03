83ee1a668e40fd747549e46e22136113d8285e02
Al Izhar Iqrami <103298330+al-izhariqrami@users.noreply.github.com>
2025-09-03 14:32:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-# import numpy as np
-# import pandas as pd
-# from io import BytesIO
-# import re
-# import tabula  # pip install tabula-py
-# import plotly.express as px
+import numpy as np
+import pandas as pd
+from io import BytesIO
+import re
+import tabula  # pip install tabula-py
+import plotly.express as px
 
 st.title("Preprocessing & Analytic Data RKA SKPD")
 
@@ -215,5 +215,6 @@ if pdf_file:
         )
     except Exception as e:
         st.error(f"Terjadi error saat membaca PDF: {e}")
+
 
 
